694f8e6805449187add780569f130657c3dbee2a
ksb-ouo <bennyrabbit01@gmail.com>
2026-01-28 16:49:27 +0900

    Add files via upload

--- back/04/Ex03.java
+++ back/04/Ex03.java
@@ -17,10 +17,12 @@ public class Ex03 {
         // scores가 int이므로, 결과를 double(소수점까지 보이도록)로 표현하기 위해 3을 3.0(double type)으로 적는다
 
         if (average >= 60) { // if문으로 평균이 60 이상일 경우, 미만일 경우를 나누기 위함
-            System.out.println("합격입니다.");
+            System.out.println("평균 점수 " + average + "로, 합격입니다.");
         } else {
-            System.out.println("불합격입니다, 다시 시험을 보세요.");
+            System.out.println("평균 점수 " + average + "로, 불합격입니다. 다시 시험을 보세요.");
         }
+
+        // 참고용
 
         // if (조건) {
         // 참일 경우
