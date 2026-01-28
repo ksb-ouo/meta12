bd38f149e802fbefcf40d960074553261351c43c
ksb-ouo <bennyrabbit01@gmail.com>
2026-01-28 10:28:43 +0900

    Add files via upload

--- back/02/Ex03.java
+++ back/02/Ex03.java
@@ -1,19 +1,18 @@
-package exam02;
 
 public class Ex03 {
 	public static void main(String[] args) {
 		// 타입 변환(형변환) : 강제형 변환(casting)
 		// 값을 변수에 담을 때 형변환이 일어날 수 있음
-		
-		int v1 = (int) 3.54;    // 실수 값을 정수 타입인 int로 강제로 바꿈 (강제형 변환, casting)
+
+		int v1 = (int) 3.54; // 실수 값을 정수 타입인 int로 강제로 바꿈 (강제형 변환, casting)
 		System.out.println(v1);
-		
-		long v2 = (long) 30;    // (자동형 변환, promotion)
+
+		long v2 = (long) 30; // (자동형 변환, promotion)
 		System.out.println(v2);
-		
+
 		long v3 = (long) 3.14f;
 		System.out.println(v3);
-		
+
 		// int + int = int
 		// int + long = long
 		// long + float = float
---
+++ back/03/Ex01.java
@@ -0,0 +1,17 @@
+public class Ex01 {
+    public static void main(String[] args) {
+
+        // 산술 연산자
+
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println(num1 + num2); // 8
+        System.out.println(num1 - num2); // 2
+        System.out.println(num1 * num2); // 15
+        System.out.println(num1 / num2); // 1 (정수 타입이기 때문에 몫만 산출)
+        System.out.println(num1 % num2); // 2 (나머지)
+        // 변수 2개, 산술 연산자 사용
+        // 변수의 타입이 같지 않다면 큰 타입으로 적용되어 산출됨
+
+    }
+}
