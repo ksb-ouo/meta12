57acf2104247789def70f0d85b95e5739017c52e
ksb-ouo <bennyrabbit01@gmail.com>
2026-02-09 15:16:02 +0900

    Add files via upload

--- back/exam07/Ex04/AddressBook.java
+++ back/exam07/Ex04/AddressBook.java
@@ -1,5 +1,19 @@
 package back.exam07.Ex04;
 
 public class AddressBook {
+    // 필드
+    String name;
+    String phone;
+    String address;
+
+    // 생성자
+
+    // 메소드
+    public void display() { // 메소드를 사용하여 반복 출력
+        // String name // 메소드 안에서 선언된 변수는 메소드 안에서만 유효한 지역 변수로 작용
+        System.out.println(name);
+        System.out.println(phone);
+        System.out.println(address);
+    }
 
 }
--- back/exam07/Ex04/AddressBookExample.java
+++ back/exam07/Ex04/AddressBookExample.java
@@ -1,5 +1,35 @@
 package back.exam07.Ex04;
 
+import java.util.Scanner;
+
 public class AddressBookExample {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("이름: ");
+        String name = sc.nextLine();
+
+        System.out.println("전화번호: ");
+        String phone = sc.nextLine();
+
+        System.out.println("주소: ");
+        String address = sc.nextLine();
+
+        sc.close();
+
+        AddressBook addressBook1 = new AddressBook();
+        addressBook1.name = name; // AddressBook의 name에 현재 입력받은 값을 저장한다.
+        addressBook1.phone = phone; //
+        addressBook1.address = address; //
+
+        System.out.println(addressBook1);
+        System.out.println(addressBook1.name);
+        System.out.println(addressBook1.phone);
+        System.out.println(addressBook1.address);
+
+        addressBook1.display();
+
+    }
 
 }
