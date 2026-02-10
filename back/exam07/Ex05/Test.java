0442da1723733f4ed3e6d496354827de5c7d380d
ksb-ouo <bennyrabbit01@gmail.com>
2026-02-10 10:30:29 +0900

    Add files via upload

--- back/exam07/Ex05/Test.java
+++ back/exam07/Ex05/Test.java
@@ -25,10 +25,37 @@ public class Test {
         int num2 = 7;
         int hab = num1 + num2;
         // System.out.println("hab: " + hab);
+        // 리턴값이 존재하므로 위와 같은 방식으로 출력하도록 메소드 안에서 정하거나, 본 클래스 안에서 메소드 결과를 변수로 선언하여 변수를 출력하는
+        // 방식 채용
         return hab; // 리턴(반환)값, 메소드 실행이 끝나면 해당 값을 내보냄
     }
 
     public int imsi_4(int a, int b) {
-        return 0;
+        int num1 = a;
+        int num2 = b;
+        int hab = num1 + num2;
+        return hab;
+    }
+
+    // 추가 1
+    public double imsi_5(double a, int b) {
+        double num1 = a;
+        int num2 = b;
+        double hab = num1 + num2;
+        return hab;
+    }
+
+    // 추가 2
+    public void imsi_6() { // 반복해서 찍는 것도 메소드를 활용하여 실행 가능
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
+        System.out.println("7");
+        System.out.println("8");
+        System.out.println("9");
+        System.out.println("10");
     }
 }
--- back/exam07/Ex05/TestExample.java
+++ back/exam07/Ex05/TestExample.java
@@ -4,22 +4,43 @@ public class TestExample {
 
     public static void main(String[] args) {
 
-        Test t1 = new Test();
+        Test t1 = new Test(); // 설계도인 Test를 사용하여 만든 t1
 
         t1.imsi_1(); // t1 안에 있는, imsi_1 메소드를 호출
 
         t1.imsi_2(1, 2); // 메소드 호출 부분, 매개변수가 2개이기 때문에 넣어주지 않으면 error
 
-        int hab = t1.imsi_3(); // 메소드 호출 부분(리턴값이 존재하므로, 메소드 안에서 sysout을 사용하여 출력하거나, 변수에 담아 출력해야 함)
-        System.out.println("hab --> " + hab);
+        int result1 = t1.imsi_3(); // 메소드 호출 부분(리턴값이 존재하므로, 메소드 안에서 sysout을 사용하여 출력하거나, 변수에 담아 출력해야 함)
+        System.out.println("합 --> " + result1);
         // 리턴값이 존재하는 메소드의 결과에만 변수를 담을 수 있음
         // 리턴값이 존재하지 않는 메소드의 결과에 변수를 담으면 error (메소드의 결과로 나오는 리턴값이 없기 때문)
 
+        int result_1 = t1.imsi_4(22, 33);
+        System.out.println("합 --> " + result_1);
+
         System.out.println();
         System.out.println();
 
-        Test t2 = new Test();
+        Test t2 = new Test(); // 설계도인 Test를 사용하여 만든 t2
+
         t2.imsi_1(); // t2 안에 있는, imsi_1 메소드를 호출
+
+        t2.imsi_2(33, 55);
+
+        int result2 = t2.imsi_3();
+        System.out.println("합 --> " + result2);
+
+        int result_2 = t2.imsi_4(55, 66);
+        System.out.println("합 --> " + result_2);
+
+        System.out.println();
+        System.out.println();
+
+        t1.imsi_6();
+        t1.imsi_6();
+        t1.imsi_6();
+        // 중복되는 내용을 반복하여 편하게 사용 가능
+
     }
 
 }
